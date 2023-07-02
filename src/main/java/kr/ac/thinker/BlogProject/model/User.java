282f253383b907876d7db334980ef3193706ea6a
thinker <jeonghun5092@gmail.com>
2023-07-03 04:25:02 +0900

    유저 impot 정리

--- src/main/java/kr/ac/thinker/BlogProject/model/User.java
+++ src/main/java/kr/ac/thinker/BlogProject/model/User.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.DynamicInsert;
-
 import javax.persistence.*;
 import java.sql.Timestamp;
 
