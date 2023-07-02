792f3cce6789d0c9b10d9e3e213e1838781c1a47
thinker <jeonghun5092@gmail.com>
2023-07-02 20:30:42 +0900

    EAGAL 전략, LAZY 전략
    이 부분은 모르는 부분들이 좀 있음.

--- src/main/java/kr/ac/thinker/BlogProject/model/Board.java
+++ src/main/java/kr/ac/thinker/BlogProject/model/Board.java
@@ -40,11 +40,19 @@ public class Board {
     // DB는 오브젝트를 저장할 수 없어서 FK 사용, 자바는 오브젝트를 저장할 수 있다.
     // private int userId;
 
-    @ManyToOne // M : 1, Many = Board : One = User
+    // 기본 전략 (EAGER 전략)
+    @ManyToOne(fetch = FetchType.EAGER) // M : 1, Many = Board : One = User
     @JoinColumn(name = "userId")
     private User user;
 
-    @OneToMany
+    // 기본 전략 (LAZY 전략)
+    // 상세 보기 창에서 댓글을 숨기지 않고, 전부 보여줄 것임 -> EAGER
+    // 펼치기 버튼을 클릭하기 전에 댓글을 보여줄 필요 없음 -> LAZY
+    // mappBy = "필드이름"
+    // DB에 컬럼을 만들지마라
+    // mappedBy 가 적혀있으면 연관관계의 주인이 아니다. (난 FK가 아니에요)
+    // 그냥 Board를 Select 할 때 join문을 통해서 값을 얻기 위해서 필요한 것이다.
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER)
     private List<Reply> reply;
 
     @CreationTimestamp
