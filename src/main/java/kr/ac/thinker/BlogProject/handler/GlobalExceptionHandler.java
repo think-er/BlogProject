9443ff69ceb7d696a222e67342a87c3061fcddda
thinker <jeonghun5092@gmail.com>
2023-07-05 14:51:59 +0900

    글로벌 에러 처리 수정
    모든 글로벌 에러에 대해서 처리할 수 있도록 하고 반환하는걸 json으로 감싸서 반환

--- src/main/java/kr/ac/thinker/BlogProject/handler/GlobalExceptionHandler.java
+++ src/main/java/kr/ac/thinker/BlogProject/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package kr.ac.thinker.BlogProject.handler;
 
+import kr.ac.thinker.BlogProject.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,7 @@ public class GlobalExceptionHandler {
     // Exception이 발생했을 때 이 함수를 실행시키려면 어디서든 Exception이 발생해도 이쪽으로 하기 위해서
     // 어노테이션 추가해야한다. -> ControllerAdvice
     @ExceptionHandler(value = Exception.class)
-    public String handleArgumentException(IllegalArgumentException e) {
-        return "<h1>" + e.getMessage() + "</h1>";
+    public ResponseDto<String> handleArgumentException(Exception e) {
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 }
