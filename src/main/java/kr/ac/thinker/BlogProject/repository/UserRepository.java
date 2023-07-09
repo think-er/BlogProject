853811e70545c5e75245b559148ba89c6e9bf6f2
thinker <jeonghun5092@gmail.com>
2023-07-09 16:57:57 +0900

    UserRepository 기능 추가

--- src/main/java/kr/ac/thinker/BlogProject/repository/UserRepository.java
+++ src/main/java/kr/ac/thinker/BlogProject/repository/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // 해당 JPA 리포지토리는 유저 테이블이 관리하고, 유저 테이블의 PK의 도메인은 Integer이다.
 // Data Access Object (DAO)
 // 자동으로 bean 등록이 된다.
@@ -18,4 +20,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 /*    // nativeQuery?????????
     @Query(value = "SELECT * FROM user WHERE userName = ?1 AND password = ?2", nativeQuery = true)
     User login(String userName, String password);*/
+
+    // SELECT * FROM user WHERE userName = 1?;
+    Optional<User> findByUserName(String userName);
 }
