547976ba8215569b1731fb33559e95f5c73ea200
thinker <jeonghun5092@gmail.com>
2023-07-09 19:24:00 +0900

    게시글 전체 목록 조회

--- src/main/java/kr/ac/thinker/BlogProject/controller/BoardController.java
+++ src/main/java/kr/ac/thinker/BlogProject/controller/BoardController.java
@@ -1,17 +1,29 @@
 package kr.ac.thinker.BlogProject.controller;
 
 import kr.ac.thinker.BlogProject.config.auth.PrincipalDetail;
+import kr.ac.thinker.BlogProject.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
+    // 컨트롤러에서 세션을 어떻게 찾는지?
+
     @GetMapping({"","/"}) // 근데 이렇게 하면 controller에서 세션을 어떻게 확인하고 다른 창을 보여주는거지?
-    public String index(@AuthenticationPrincipal PrincipalDetail principal) {
-        // /WEB-INF/views/index(.jsp)
-        System.out.println("로그인 사용자 아이디 : " + principal.getUsername());
-        return "index";
+    public String index(Model model) {
+        model.addAttribute("boards", boardService.getList());
+        return "index"; // viewResolver 작동!!!
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -2,31 +2,18 @@
 
 <%@ include file="layout/header.jsp"%>
 
+<%-- 한 건씩 꺼내서 "board"라는 변수에 집어넣는다 --%>
+<c:forEach var="board" items="${boards}">
 <div class="container">
     <div class="card m-2">
         <div class="card-body">
-            <h4 class="card-title">제목 적는 부분</h4>
-            <a href="#" class="btn btn-primary">상세 보기</a>
+            <%-- getTitle() 이라는 메서드 호출 (내가 이미 만듬) --%>
+            <h4 class="card-title">${board.title}</h4>
+            <a href="#" class="btn btn-primary">상세보기</a>
         </div>
     </div>
 </div>
-<div class="container">
-    <div class="card m-2">
-        <div class="card-body">
-            <h4 class="card-title">제목 적는 부분</h4>
-            <a href="#" class="btn btn-primary">상세 보기</a>
-        </div>
-    </div>
-</div>
-<div class="container">
-    <div class="card m-2">
-        <div class="card-body">
-            <h4 class="card-title">제목 적는 부분</h4>
-            <a href="#" class="btn btn-primary">상세 보기</a>
-        </div>
-    </div>
-</div>
-
+</c:forEach>
 <%@ include file="layout/footer.jsp"%>
 
 
